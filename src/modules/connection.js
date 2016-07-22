b00b0fdc05ad3afd3bac6f8535b9ff3acb75e0f2
Mohammad Rajabifard <mo.rajbi@gmail.com>
2016-07-22 23:17:12 +0430

    Removed server address

--- src/modules/connection.js
+++ src/modules/connection.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
+const SERVER_ADDRESS = 'http://localhost:8888';
+
 const options = {
 	method: 'GET',
-	url: 'http://178.63.163.126:3012/tweets?search=irDevConf&limit=100',
-	headers: {
-		token: "fb9f985d48858b5d49a6e5014b568fde"
-	}
+	url: SERVER_ADDRESS + '/tweets?search=irDevConf&limit=100',
 };
 
 const request = axios(options);
